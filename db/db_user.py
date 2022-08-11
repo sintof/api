605e0dbf4c29de71d94e9ac13a4a1827a2645ffd
sintof <dcpersonf@gmail.com>
2022-08-11 19:51:48 +0500

    added auth

--- db/db_user.py
+++ db/db_user.py
@@ -2,6 +2,7 @@ from db.hash import Hash
 from sqlalchemy.orm.session import Session
 from schemas import UserBase
 from db.models import DbUser
+from fastapi import HTTPException, status
 
 
 def create_user(db: Session, request: UserBase):
@@ -19,13 +20,37 @@ def get_all_users(db: Session):
   return db.query(DbUser).all()
 
 def get_user(db: Session, id: int):
-  return db.query(DbUser).filter(DbUser.id == id).first()
+  user = db.query(DbUser).filter(DbUser.id == id).first()
+  if not user:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+      detail=f'User with id {id} not found')
+  return user
+
+def get_user_by_username(db: Session, username: str):
+  user = db.query(DbUser).filter(DbUser.username == username).first()
+  if not user:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+      detail=f'User with username {username} not found')
+  return user
 
 def update_user(db: Session, id: int, request: UserBase):
-  user = db.query(DbUser).filter(DbUser.id == id).first()
+  user = db.query(DbUser).filter(DbUser.id == id)
+  if not user.first():
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+      detail=f'User with id {id} not found')
   user.update({
     DbUser.username: request.username,
     DbUser.email: request.email,
     DbUser.password: Hash.bcrypt(request.password)
   })
-  db.commit() # Commit the changes to the database
+  db.commit()
+  return 'ok'
+
+def delete_user(db: Session, id: int):
+  user = db.query(DbUser).filter(DbUser.id == id).first()
+  if not user:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+      detail=f'User with id {id} not found')
+  db.delete(user)
+  db.commit()
+  return 'ok'
